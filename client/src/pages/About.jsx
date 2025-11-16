ff263a1155da82593be15b06bd784c58701dd616
Yuvrajsinh Chauhan <yuvrajsinhchauhan047@gmail.com>
2025-11-16 11:56:06 +0530

    Add About page with project details

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container">
+      <h2>About CodeLearn</h2>
+      <p>
+        CodeLearn एक learning platform है जहाँ आप programming languages 
+        को simple और आसान तरीके से सीख सकते हो।
+      </p>
+      <p>
+        यह project React + Django + MySQL का use करके बनाया गया है।
+      </p>
+    </div>
+  );
+}
+
+export default About;
