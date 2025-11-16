8e0a4dbf4cb38bbd20961289ec139e7d546f10c4
Yuvrajsinh Chauhan <yuvrajsinhchauhan047@gmail.com>
2025-11-16 11:57:06 +0530

    Add Contact page with contact information

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="container">
+      <h2>Contact Us</h2>
+      <p>You can contact us anytime at:</p>
+
+      <h3>Email:</h3>
+      <p>support@codelearn.com</p>
+
+      <h3>Phone:</h3>
+      <p>+91 98765 43210</p>
+
+      <h3>Address:</h3>
+      <p>Ahmedabad, Gujarat, India</p>
+    </div>
+  );
+}
+
+export default Contact;
