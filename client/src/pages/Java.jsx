dab174bae317faa03a5793cc8f5f35adc9ae8a26
Yuvrajsinh Chauhan <yuvrajsinhchauhan047@gmail.com>
2025-11-16 12:01:48 +0530

    Add Java component with programming overview

---
+++ client/src/pages/Java.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Java() {
+  return (
+    <div className="container">
+      <h2>Java Programming</h2>
+      <p>
+        Java is an object-oriented, class-based programming language used for
+        Android apps, backend systems, enterprise software, and more.
+      </p>
+
+      <h3>Example:</h3>
+
+      <pre className="code-block">
+{`class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+  }
+}`}
+      </pre>
+    </div>
+  );
+}
+
+export default Java;
